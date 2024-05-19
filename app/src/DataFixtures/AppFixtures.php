8c9da4c7f5f3541dd13f93eda331614b7200c9bb
Michał Dudek <michalpaweldudek@gmail.com>
2024-05-19 12:53:29 +0200

    pagination finished

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * This file is part of the notes-and-to-do-app.
+ *
+ * (c) [Michał Dudek] <michalpawel.dudek@student.uj.edu.pl>
+ */
+
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * AppFixtures.
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * Load data fixtures with the passed EntityManager.
+     *
+     * @param ObjectManager $manager Manager
+     */
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
--- app/src/DataFixtures/TaskFixtures.php
+++ app/src/DataFixtures/TaskFixtures.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Task fixtures.
+ *
+ * This file is part of the notes-and-to-do-app.
+ *
+ * (c) [Michał Dudek] <michalpawel.dudek@student.uj.edu.pl>
  */
 
 namespace App\DataFixtures;
 
 use App\Entity\Task;
-use DateTimeImmutable;
-use Doctrine\Persistence\ObjectManager;
 
 /**
  * Class TaskFixtures.
@@ -19,14 +21,14 @@ class TaskFixtures extends AbstractBaseFixtures
      */
     public function loadData(): void
     {
-        for ($i = 0; $i < 10; ++$i) {
+        for ($i = 0; $i < 30; ++$i) {
             $task = new Task();
             $task->setTitle($this->faker->sentence);
             $task->setCreatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
             );
             $task->setUpdatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
             );
             $this->manager->persist($task);
         }
