a32df89d2b020651b2b74fa4be756ab83307551e
Michał Dudek <michalpaweldudek@gmail.com>
2024-05-19 15:30:24 +0200

    doctrine optimization

--- app/src/Entity/Task.php
+++ app/src/Entity/Task.php
@@ -51,7 +51,7 @@ class Task
     *
     * @var Category
      */
-    #[ORM\ManyToOne(targetEntity: Category::class)]
+    #[ORM\ManyToOne(targetEntity: Category::class, fetch: 'EXTRA_LAZY')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
 
